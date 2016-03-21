8f3ee8fcc02a5f8577f06085110da822b462fee7
Dan <Dan>
2016-03-21 18:48:35 -0400

    changed several things in terms of what is being displayed. Added a check to the search query to prevent searching with nulls/ empty strings. Did not do any specific back-end modifications
    Changed several things in terms of what is being displayed. Added a check
    to the search query to prevent searching with nulls/ empty strings. Did
    not do any specific or major changes to the back-end.

--- node/spotify_playlist/routes/index.js
+++ node/spotify_playlist/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+/* this webside might be helpful for login: http://jsfiddle.net/JMPerez/62wafrm7/   */
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Spotify Queue' });
 });
 
 module.exports = router;
--- node/spotify_playlist/routes/songs.js
+++ node/spotify_playlist/routes/songs.js
@@ -7,17 +7,31 @@ var bodyParser = require('body-parser');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var url = 'https://api.spotify.com/v1/search?q=' + req.query.q +"&type=track&market=US"
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      // console.log(body) // Show the HTML for the Google homepage.
-      var json_body = JSON.parse(body)
-      console.log(json_body.tracks.items)
-      res.render('song', { tracks: json_body.tracks.items});
-    }
-  });
+  // --> not implemented yet
+  // call (here/below this line) res.render to some view that will add a search field + search button
+  // --> not implemented yet
 
+  // results of possibly already made searches
+  if ((req.query.q != null) && ((req.query.q).length > 0))  {         // search field checks out
+    // var url = 'https://api.spotify.com/v1/search?q=' +"\"" +req.query.q +"\"" +"&type=track&market=US"; <-- for more "specific searches"
+    var url = 'https://api.spotify.com/v1/search?q=' + req.query.q + "&type=track&market=US";
 
+    request(url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        // console.log(body) // Show the HTML for the Google homepage.
+        var json_body = JSON.parse(body)
+        console.log(json_body.tracks.items)
+
+        res.render('song', {tracks: json_body.tracks.items});
+
+      }
+    });
+
+  }
+  else{
+    // no search has been given
+    res.render('song', {tracks: ""});
+  }
 });
 
 module.exports = router;
