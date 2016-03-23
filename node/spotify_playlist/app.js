7fb1b6f073901d6cf214d229dacf26ef2e48f933
Ernest Dever <e@ernestdever.com>
2016-03-22 23:03:29 -0400

    Search working
    I think i got search to work a bit better, it renders the correct page,
    at least

--- node/spotify_playlist/app.js
+++ node/spotify_playlist/app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var songs = require('./routes/songs');
+var searches = require('./routes/search');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(function(req, res, next) {
 app.use('/', routes);
 app.use('/users', users);
 app.use('/songs', songs);
+app.use('/search', searches);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
