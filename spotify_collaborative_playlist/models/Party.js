97ba2f95b3e1b222d6d71684dc6e60563f3f07f1
Ernest Dever <e@ernestdever.com>
2016-04-21 13:10:43 -0400

    show song name instead of song id

--- spotify_collaborative_playlist/models/Party.js
+++ spotify_collaborative_playlist/models/Party.js
@@ -5,7 +5,9 @@ var PartySchema = new mongoose.Schema({
     //id: String,              // party id
     song_ids: [{            // changed to an object with 2 different values: songid and votes; may need to be changed to the entire song object + votes
         songid : String,
-        votes : Number
+        votes : Number,
+        name: String,
+        artists: String
     }],
     user_ids: Array,     // array of user id's that have joined the party
     host_id: String
