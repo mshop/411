06a618a73cafc2a8ceedf2cdc93f4e4a232b24a1
Dan <Dan>
2016-04-24 03:52:43 -0400

    Added some security protocals to both the search for party id as well as the url that is involved when getting the party id. This fix has (should have) solved several bugs, including: special characters in the search bar (?,_,ect), errors and even app crashing when requesting a party id that does not exist, party ids of incorrect length or form inside the url, and several other bugs of these type.

--- spotify_collaborative_playlist/controllers/home.js
+++ spotify_collaborative_playlist/controllers/home.js
@@ -10,8 +10,22 @@ exports.index = function(req, res) {
     //console.log(req.url);
     //console.log(req.query.query);
     if(req.query.query != null){
-      // console.log("woohoo!");
-      res.redirect('/party/' + req.query.query);
+      if (req.query.query.length == 24) {
+        var reg = /^[a-z0-9]+$/i;
+        console.log(reg.test(req.query.query));
+        // console.log("woohoo!");
+        if (!(reg.test(req.query.query))) {
+          req.flash('errors', {msg: "party id must be alpha-numerical; cannot contain special characters."});
+          res.redirect('/');
+        }
+        else {
+          res.redirect('/party/' + req.query.query);
+        }
+      }
+      else{
+        req.flash('errors', {msg: "party id must be exactly 24 characters long."});
+        res.redirect('/');
+      }
     }
     else {
       //console.log(req.user);
